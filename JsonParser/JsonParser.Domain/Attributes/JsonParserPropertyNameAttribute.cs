33503b704dae2426a8f7efbc08a3ebcabf898fa5
InVanSav <savickiy.iv@dns-shop.ru>
2024-07-22 19:51:53 +1000

    Создан аттрибут для кастомного парсера

---
+++ JsonParser/JsonParser.Domain/Attributes/JsonParserPropertyNameAttribute.cs
@@ -0,0 +1,21 @@
+namespace JsonParser.JsonParser.Domain.Attributes;
+
+/// <summary>
+/// Кастомный аттрибут для сопоставления имени свойства при сериализации JSON
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class JsonParserPropertyNameAttribute : Attribute
+{
+    /// <summary>
+    /// Название свойства
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// <inheritdoc cref="JsonParserPropertyNameAttribute"/>
+    /// </summary>
+    public JsonParserPropertyNameAttribute(string name)
+    {
+        Name = name;
+    }
+}
